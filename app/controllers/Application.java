e47b52c8071922baaa48711ef82d6a745ee028bf
Dan Funk <daniel.h.funk@gmail.com>
2012-03-04 22:25:13 -0500

    a few more changes.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,11 @@
 package controllers;
 
-import play.*;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import play.Logger;
+import play.libs.Mail;
 import play.mvc.*;
 
-import java.util.*;
-
-import models.*;
 
 public class Application extends Controller {
 
@@ -19,6 +19,28 @@ public class Application extends Controller {
     public static void webdev() { render(); }
     public static void analysis() { render();}
     public static void about() { render();}
-    public static void contact() { render();}
+
+    /**
+     * Handles the form input from the contact us page.
+     */
+    public static void sendMessage() {
+        String   name      = params.get("name");
+        String   address   = params.get("email");
+        String   message   = params.get("message");
+
+        SimpleEmail email = new SimpleEmail();
+        try {
+            email.setFrom("dan@lightcastletech.com");
+            email.addTo("tamara@lightcastletech.com");
+            email.addTo("dan@lightcastletech.com");
+            email.setSubject("someone filled out the form on our website.");
+            email.setMsg("Email: " + address + "\n" +
+                         "Name: " + name + "\n" +
+                         "Message: " + message );
+        } catch(EmailException e) {
+            Logger.error(e, "Failed to create email!");
+        }
+        Mail.send(email);
+    }
 
 }
